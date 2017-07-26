ed562fd4291b64f52f930df054276e2ffd1cfa1c
System <none>
2017-07-26 19:54:10 +0000

    Checkpoint

--- app/database.js
+++ app/database.js
@@ -6,7 +6,18 @@ var url = "mongodb://jordan:mack@ds149551.mlab.com:49551/fcc-backend"
 
 
 function returnUrl(long){
-
+  
+  MongoClient(url, function(err, db){
+  if (err) throw err
+  
+  var collection = db.collection('short-urls');
+    
+    
+   if ( collection.find( { "real-URL" : long } ) )
+  
+  db.close();
+});
+  
 }
 
 
@@ -23,10 +34,3 @@ function returnUrl(long){
 
 
 
-MongoClient(url, function(err, db){
-  if (err) throw err
-  
-  var collection = db.collection('short-urls');
-  console.log(collection);
-  db.close();
-});
